253fb491d2eb18de37e9ddfce1e2032f671f8aa1
NANASHISHIROUTO <gudtjs1004sd@naver.com>
2024-11-04 21:38:35 +0900

    backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,20 +17,20 @@ import { Board } from './board/entities/board.entity';
     ConfigModule.forRoot({
       isGlobal: true, // ConfigModule을 전역 모듈로 설정
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [TeamMember, LocalSemester, Board], // 엔티티를 여기에 추가
-        synchronize: true,
-      }),
-    }),
+    // TypeOrmModule.forRootAsync({
+      // imports: [ConfigModule],
+      // inject: [ConfigService],
+      // useFactory: (configService: ConfigService) => ({
+      //   type: configService.get<string>('DB_TYPE') as 'mysql',
+      //   host: configService.get<string>('DB_HOST'),
+      //   port: configService.get<number>('DB_PORT'),
+      //   username: configService.get<string>('DB_USERNAME'),
+      //   password: configService.get<string>('DB_PASSWORD'),
+      //   database: configService.get<string>('DB_NAME'),
+      //   entities: [TeamMember, LocalSemester, Board], // 엔티티를 여기에 추가
+      //   synchronize: true,
+      // }),
+    // }),
     TeamMembersModule,
     LocalSemesterModule,
     BoardModule,
